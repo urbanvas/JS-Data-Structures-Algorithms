51594428aff586eb6cd03cfcf9db81ded9db3b16
vasu <nvseejoor@gmail.com>
2019-10-11 17:07:06 -0400

    ll

--- dataStructures/doublyLinkedList.js
+++ dataStructures/doublyLinkedList.js
@@ -59,4 +59,90 @@ class DoublyLinkedList {
 		--this.length;
 		return node;
 	}
+
+	unshift(val) {
+		const node = new Node(val);
+		if (this.length === 0) {
+			this.head = 0;
+			this.tail = 0;
+		} else {
+			node.next = this.head;
+			this.head.prev = node;
+			this.head = node;
+		}
+		++this.length;
+		return this;
+	}
+
+	get(idx) {
+		if (idx <= 0 || idx >= this.length) {
+			return null;
+		}
+		let count;
+		let curr;
+		if (idx <= this.length / 2) {
+			count = 0;
+			curr = this.head;
+			while (count !== idx) {
+				curr = curr.next;
+				++count;
+			}
+		} else {
+			count = this.length - 1;
+			curr = this.tail;
+			while (count !== idx) {
+				curr = curr.prev;
+				--count;
+			}
+		}
+		return curr;
+	}
+	set(idx, val) {
+		const node = this.get(idx);
+		if (node !== null) {
+			node.val = val;
+			return true;
+		}
+		return false;
+	}
+
+	insert(idx, val) {
+		if (idx < 0 || idx > this.length) {
+			return false;
+		} else if (idx === 0) {
+			return this.unshift(val);
+		} else if (idx === this.length) {
+			return this.push(val);
+		}
+		const prev = this.get(idx - 1);
+		const curr = prev.next;
+		const node = new Node(val);
+
+		prev.next = node;
+		node.prev = prev;
+
+		node.next = curr;
+		curr.prev = node;
+
+		++this.length;
+		return true;
+	}
+
+	remove(idx) {
+		if (idx < 0 || idx >= this.length) {
+			return undefined;
+		} else if (idx === 0) {
+			return this.shift(idx);
+		} else if (idx === this.length - 1) {
+			return this.pop(idx);
+		} else {
+			const node = this.get(idx);
+			node.prev.next = node.next;
+			node.next.prev = node.prev;
+			node.next = null;
+			node.prev = null;
+			--this.length;
+			return node;
+		}
+	}
 }
