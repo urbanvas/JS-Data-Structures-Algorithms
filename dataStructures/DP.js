db3e888ea0f723bc0d0fe7dfdf1946ebf51ef46f
vasu <nvseejoor@gmail.com>
2019-10-19 20:15:33 -0400

    dp

--- dataStructures/DP.js
+++ dataStructures/DP.js
@@ -9,3 +9,16 @@ const fib = (n, memo = [ undefined, 1, 1 ]) => {
 	memo[n] = result;
 	return result;
 };
+
+// Tabulated Version
+
+const fib = (n) => {
+	if (n <= 2) {
+		return 1;
+	}
+	const fibNums = [ 0, 1, 1 ];
+	for (let i = 3; i <= n; i++) {
+		fibNums[i] = fibNums[i - 1] + fibNums[i - 2];
+	}
+	return fibNums[n];
+};
